654e95f64af0f2e820194207663ed21b84086d0c
MarivaldoDev <pedromarivaldo10@gmail.com>
2024-05-25 14:41:07 -0300

    'create-model'

--- library/models.py
+++ library/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+class Perfil(models.Model):
+    nome = models.CharField(max_length=100)
+    matricula = models.CharField(max_length=7)
+    curso = models.CharField(max_length=30)
+    turma = models.CharField(max_length=10)
+    data_criacao = models.DateField(default=date.today().strftime('%d/%m/%Y'))
+    imagem = models.ImageField(blank=True, upload_to='imagens/')
+
+    def __str__(self) -> str:
+        return f'{self.nome}'

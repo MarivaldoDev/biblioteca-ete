90e881a7a8db9861fdad0eac5f89858d3a89853b
MarivaldoDev <pedromarivaldo10@gmail.com>
2024-12-19 16:54:32 -0300

    voltando com sqlite

--- ete/settings.py
+++ ete/settings.py
@@ -80,15 +80,10 @@ WSGI_APPLICATION = 'ete.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# filepath: /path/to/your/settings.py
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'biblioteca',
-        'USER': 'root',
-        'PASSWORD': '21790',
-        'HOST': 'localhost',
-        'PORT': '3306',  # geralmente 3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

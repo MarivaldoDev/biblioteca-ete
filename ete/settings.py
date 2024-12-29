9faf57bfad311798c0fb4661367871850d5112e4
MarivaldoDev <pedromarivaldo10@gmail.com>
2024-12-28 23:28:10 -0300

    adicionando template para erro 404

--- ete/settings.py
+++ ete/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = (
     BASE_DIR / 'base_static',
 )

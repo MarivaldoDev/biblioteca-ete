e38c42e9cfb8ea839bff5a45c2186dc7e0f07069
MarivaldoDev <pedromarivaldo10@gmail.com>
2024-12-30 10:32:13 -0300

    tentando carregar media

--- ete/urls.py
+++ ete/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('', include('library.urls')),
 ]
 
-if not settings.DEBUG:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = 'library.views.not_found'

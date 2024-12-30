743535842f0d07efee7d52e061c62076a1789caa
MarivaldoDev <pedromarivaldo10@gmail.com>
2024-12-29 23:25:43 -0300

    paginação adicionada

--- ete/urls.py
+++ ete/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', include('library.urls')),
 ]
 
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
